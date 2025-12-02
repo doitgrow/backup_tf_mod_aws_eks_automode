fa4569ba69acd716b4f426b816a35a910b1f8388
vsax <admin@lge.com>
2025-12-02 10:00:03 +0900

    chores: add output

--- modules/eks-auto-mode/outputs.tf
+++ modules/eks-auto-mode/outputs.tf
@@ -1,3 +1,11 @@
-output "eks" {
-  value = module.eks
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "openid_connect_provider_url" {
+  value = module.eks.cluster_oidc_issuer_url
+}
+
+output "openid_connect_provider_arn" {
+  value = module.eks.oidc_provider_arn
 }
